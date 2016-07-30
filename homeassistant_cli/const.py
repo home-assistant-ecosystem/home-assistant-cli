34d16da143c24497f6831f34758f54e3b63c8614
Fabian Affolter <fabian@affolter-engineering.ch>
2016-07-30 12:21:26 +0200

    Add connection consts

---
+++ homeassistant_cli/const.py
@@ -0,0 +1,8 @@
+"""Constants used by Home Assistant CLI (hass-cli)."""
+PACKAGE_NAME = 'homeassistant_cli'
+__version__ = '0.0.1'
+
+# Communication details
+DEFAULT_HOST = '127.0.0.1'
+DEFAULT_PORT = 8123
+TIMEOUT = 5
