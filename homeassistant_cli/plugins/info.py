ffca83b73024c1d77cc7fe84b0af58abda2cae28
Linus Wallgren <linus.wallgren@gmail.com>
2017-08-04 00:52:34 +0200

    info now displays a nice table
    It appears that the homeassistant remote API does nt give a json
    serializable object as its config.

--- homeassistant_cli/plugins/info.py
+++ homeassistant_cli/plugins/info.py
@@ -1,11 +1,12 @@
 """Configuration plugin for Home Assistant CLI (hass-cli)."""
 import webbrowser
 import urllib.parse
+from collections import namedtuple
 
 import click
 
 from homeassistant_cli.cli import pass_context
-from homeassistant_cli.helper import json_output, timestamp, req
+from homeassistant_cli.helper import timestamp
 
 OSM_URL = 'https://www.openstreetmap.org'
 ZOOM = 17
@@ -20,7 +21,7 @@ def cli(ctx, location, connect):
     config = remote.get_config(ctx.api)
 
     if not location and not connect:
-        ctx.log(json_output(config))
+        ctx.log(format_config(config))
         ctx.vlog('Details of %s, Recieved: %s', ctx.host, timestamp())
 
     if location and not connect:
@@ -32,3 +33,27 @@ def cli(ctx, location, connect):
         url = 'http://{}:{}'.format(ctx.host, ctx.port)
         webbrowser.open_new_tab(url)
 
+def format_config(config):
+    UnitSystem = namedtuple('UnitSystem', ['mass', 'volume', 'distance', 'temperature'])
+    units = namedtuple('UnitSystem', config.get('unit_system').keys())(**config.get('unit_system'))
+    format_arguments = {
+            **config,
+            'whitelist_external_dirs': ', '.join(config.get('whitelist_external_dirs')),
+            'units': units,
+            'components': ', '.join(sorted(config.get('components'))),
+            }
+    return """
+Location name:                      {location_name}
+Version:                            {version}
+Location:                           {latitude}°, {longitude}°
+Elevation:                          {elevation}{units.length}
+Timezone:                           {time_zone}
+Whitelisted external directories:   {whitelist_external_dirs}
+Config dir:                         {config_dir}
+Units:
+    Mass:                           {units.mass}
+    Volume:                         {units.volume}
+    Distance:                       {units.length}
+    Temperature:                    {units.temperature}
+Components:                         {components}
+    """.strip().format(**format_arguments)
