49d69cd4dc39cf2cd176698ad6725c70fe980b94
Max Rydahl Andersen <max.andersen@gmail.com>
2019-01-07 16:41:37 +0100

    fix so quotes are preserved

--- homeassistant_cli/plugins/lovelace-gen.py
+++ homeassistant_cli/plugins/lovelace-gen.py
@@ -1,13 +1,16 @@
+import collections
 import logging
 import os
+from pathlib import Path
 import sys
 import time
 
 import click
 from homeassistant_cli.cli import pass_context
 import jinja2
+import ruamel.yaml
 from ruamel.yaml import YAML
-from ruamel.yaml.constructor import SafeConstructor
+from ruamel.yaml.constructor import RoundTripConstructor, SafeConstructor
 
 _LOGGING = logging.getLogger(__name__)
 MAIN_FILE = 'main.yaml'
@@ -16,6 +19,17 @@ GENERATOR_MESSAGE = """
 # https://github.com/thomasloven/homeassistant-lovelace-gen
 # Any changes made to it will be overwritten the next time the script is run.
 """
+
+
+def my_compose_document(self):
+    self.get_event()
+    node = self.compose_node(None, None)
+    self.get_event()
+    # self.anchors = {}    # <<<< commented out
+    return node
+
+
+ruamel.yaml.RoundTripLoader.compose_document = my_compose_document
 
 
 def get_input_dir(inp):
@@ -38,6 +52,10 @@ def process_file(jinja, yaml, path):
 
 def include_statement(loader, node):
     global jinja, yaml
+    y = loader.loader
+    yaml = YAML(typ=y.typ, pure=y.pure)  # same values as including YAML
+    yaml.preserve_quotes = True
+    yaml.composer.anchors = loader.composer.anchors
     return process_file(jinja, yaml, node.value)
 
 
@@ -73,15 +91,20 @@ def cli(ctx, input, output):
 
     global jinja, yaml, inp
 
+    #    ruamel.yaml.representer.RoundTripRepresenter.add_representer(
+    #        collections.OrderedDict,
+    #        ruamel.yaml.representer.RoundTripRepresenter.represent_ordereddict)
+
     inp = get_input_dir(input)
     outp = output or os.path.join(inp, '..', 'ui-lovelace.yaml')
 
     jinja = jinja2.Environment(loader=jinja2.FileSystemLoader(inp))
-    yaml = YAML(typ='safe')
-    yaml.Constructor = SafeConstructor
+    yaml = YAML(typ='rt')
+    yaml.preserve_quotes = True
+    yaml.Constructor = RoundTripConstructor
 
-    SafeConstructor.add_constructor("!include", include_statement)
-    SafeConstructor.add_constructor("!file", file_statement)
+    RoundTripConstructor.add_constructor("!include", include_statement)
+    RoundTripConstructor.add_constructor("!file", file_statement)
 
     try:
         data = process_file(jinja, yaml, MAIN_FILE)
@@ -95,6 +118,6 @@ def cli(ctx, input, output):
             fp.write(GENERATOR_MESSAGE)
             yaml.dump(data, fp)
     except Exception as e:
-        _LOGGING.error("Writing ui-lovelace.yaml failed.", file=sys.stderr)
+        _LOGGING.error("Writing ui-lovelace.yaml failed.")
         _LOGGING.exception(e)
         sys.exit(4)
