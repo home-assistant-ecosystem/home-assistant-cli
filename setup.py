92a41053ef2a6425bb93ed6e2e701e5a76613cf5
Fabian Affolter <mail@fabian-affolter.ch>
2019-02-11 09:44:58 +0100

    Upgrade mypy to 0.670 (#159)

--- setup.py
+++ setup.py
@@ -117,7 +117,7 @@ TESTS_REQUIRE = [
     'flake8-docstrings==1.3.0',
     'flake8==3.6.0',
     'mock-open==1.3.1',
-    'mypy==0.660',
+    'mypy==0.670',
     'pydocstyle==2.1.1',
     'pylint==2.2.2',
     'pytest-cov==2.6.1',
