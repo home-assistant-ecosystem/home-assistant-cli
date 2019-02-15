a51ecf7d0abc2fa94ab8a65f309c55ed63eb1d27
Fabian Affolter <mail@fabian-affolter.ch>
2019-02-15 13:28:26 +0100

    Add support for system health (#172)
    * Add support for system health
    
    * fix(health): proper formatting of system health

--- homeassistant_cli/plugins/system.py
+++ homeassistant_cli/plugins/system.py
@@ -3,6 +3,9 @@ import logging
 
 import click
 from homeassistant_cli.cli import pass_context
+from homeassistant_cli.config import Configuration
+import homeassistant_cli.const as const
+from homeassistant_cli.helper import format_output
 import homeassistant_cli.remote as api
 
 _LOGGING = logging.getLogger(__name__)
@@ -19,3 +22,18 @@ def cli(ctx):
 def log(ctx):
     """Get errors from Home Assistant."""
     click.echo(api.get_raw_error_log(ctx))
+
+
+@cli.command()
+@pass_context
+def health(ctx: Configuration):
+    """Get system health from Home Assistant."""
+    info = api.get_health(ctx)
+
+    ctx.echo(
+        format_output(
+            ctx,
+            [info],
+            columns=ctx.columns if ctx.columns else const.COLUMNS_DEFAULT,
+        )
+    )
