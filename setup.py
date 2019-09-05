8556d746477346394e78b122837b757287075427
Fabian Affolter <mail@fabian-affolter.ch>
2019-09-05 14:46:32 +0200

    Fix for click

--- setup.py
+++ setup.py
@@ -81,7 +81,7 @@ PACKAGES = find_packages(exclude=['tests', 'tests.*'])
 REQUIRES = [
     'aiohttp>=3.5.4,<4',
     'click-log>=0.3.2,<0.4',
-    'click==7.0,<8',
+    'click>=7.0,<8',
     'dateparser>=0.7.1,<0.8',
     'jinja2>=2.10',
     'jsonpath-rw>=1.4.0,<2',
