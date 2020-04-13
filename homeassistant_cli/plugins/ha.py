7c9c1d78849a50dee7a9430460bcf0ee1c541f73
Fabian Affolter <mail@fabian-affolter.ch>
2020-04-13 13:54:23 +0200

    Add new commands

--- homeassistant_cli/plugins/ha.py
+++ homeassistant_cli/plugins/ha.py
@@ -4,13 +4,18 @@ import logging
 from typing import Any, Dict, List, cast  # noqa: F401
 
 import click
-
+from requests.exceptions import HTTPError
+from distutils.version import StrictVersion
 from homeassistant_cli.cli import pass_context
 from homeassistant_cli.config import Configuration
 from homeassistant_cli.helper import format_output
 import homeassistant_cli.remote as api
+from homeassistant_cli.exceptions import HomeAssistantCliError
 
 _LOGGING = logging.getLogger(__name__)
+
+# These commands loosely based on what is found in
+# https://github.com/home-assistant/supervisor/blob/master/API.md
 
 
 @click.group('ha')
@@ -39,13 +44,21 @@ def _report(ctx, cmd, method, response) -> None:
         )
 
 
-def _handle(ctx, method, httpmethod='get') -> None:
+def _handle(ctx, method, httpmethod='get', raw=False) -> None:
     """Handle the data."""
     method = "/api/hassio/" + method
     response = api.restapi(ctx, httpmethod, method)
     api.restapi(ctx, httpmethod, method)
 
     _report(ctx, httpmethod, method, response)
+
+
+def _handle_raw(ctx, method, httpmethod='get') -> Dict:
+    """Handle raw data."""
+    method = "/api/hassio/" + method
+    response = api.restapi(ctx, httpmethod, method)
+    api.restapi(ctx, httpmethod, method)
+    return response.json()
 
 
 @cli.group('supervisor')
@@ -55,25 +68,35 @@ def supervisor(ctx: Configuration):
     ctx.auto_output("data")
 
 
-@supervisor.command()
+@supervisor.command('ping')
 @pass_context
-def ping(ctx: Configuration):
+def supervisor_ping(ctx: Configuration):
     """Home Assistant supervisor ping."""
     _handle(ctx, 'supervisor/ping')
 
 
-@supervisor.command()
+@supervisor.command('info')
 @pass_context
-def info(ctx: Configuration):
+def supervisor_info(ctx: Configuration):
     """Home Assistant supervisor info."""
     _handle(ctx, 'supervisor/info')
 
 
-@supervisor.command()
+@supervisor.command('update')
 @pass_context
-def update(ctx: Configuration):
+def supervisor_update(ctx: Configuration):
     """Home Assistant supervisor update."""
-    _handle(ctx, 'supervisor/update', 'post')
+    response = _handle_raw(ctx, 'supervisor/info')
+    data = response['data']
+    current_version = int(data['version'])
+    latest_version = int(data['version_latest'])
+    if current_version == latest_version:
+        ctx.echo("Already running the latest release")
+    else:
+        try:
+            _handle(ctx, 'supervisor/update', 'post')
+        except (HomeAssistantCliError, HTTPError):
+            pass
 
 
 @supervisor.command('options')
@@ -90,18 +113,25 @@ def supervisor_reload(ctx: Configuration):
     _handle(ctx, 'supervisor/reload', 'post')
 
 
-@supervisor.command()
+@supervisor.command('logs')
 @pass_context
-def logs(ctx: Configuration):
+def supervisor_logs(ctx: Configuration):
     """Home Assistant supervisor logs."""
     _handle(ctx, 'supervisor/logs')
 
 
-@supervisor.command()
+@supervisor.command('repair')
 @pass_context
-def repair(ctx: Configuration):
+def supervisor_repair(ctx: Configuration):
     """Home Assistant supervisor repair."""
     _handle(ctx, 'supervisor/repair')
+
+
+@supervisor.command('stats')
+@pass_context
+def supervisor_stats(ctx: Configuration):
+    """Home Assistant supervisor stats."""
+    _handle(ctx, 'supervisor/stats')
 
 
 @cli.group('snapshot')
@@ -118,9 +148,9 @@ def snapshot_reload(ctx: Configuration):
     _handle(ctx, 'snapshots/reload', 'post')
 
 
-@snapshot.command()
+@snapshot.command('shutdown')
 @pass_context
-def shutdown(ctx: Configuration):
+def snapshot_shutdown(ctx: Configuration):
     """Home Assistant host shutdown."""
     _handle(ctx, 'host/shutdown', 'post')
 
@@ -132,9 +162,9 @@ def host(ctx: Configuration):
     ctx.auto_output('data')
 
 
-@host.command()
+@host.command('reboot')
 @pass_context
-def reboot(ctx: Configuration):
+def host_reboot(ctx: Configuration):
     """Home Assistant host reboot."""
     _handle(ctx, 'host/reboot', 'post')
 
@@ -146,30 +176,30 @@ def host_reload(ctx: Configuration):
     _handle(ctx, 'host/reload', 'post')
 
 
-@host.command()
+@host.command('shutdown')
 @pass_context
-def shutdown(ctx: Configuration):
+def host_shutdown(ctx: Configuration):
     """Home Assistant host shutdown."""
     _handle(ctx, 'host/shutdown', 'post')
 
 
-@host.command()
+@host.command('info')
 @pass_context
-def info(ctx: Configuration):
-    """Home Assistant host shutdown."""
+def host_info(ctx: Configuration):
+    """Home Assistant host info."""
     _handle(ctx, 'host/info')
 
 
-@host.command()
+@host.command('options')
 @pass_context
-def options(ctx: Configuration):
+def host_options(ctx: Configuration):
     """Home Assistant options shutdown."""
     _handle(ctx, 'host/options', 'post')
 
 
-@host.command()
+@host.command('services')
 @pass_context
-def services(ctx: Configuration):
+def host_services(ctx: Configuration):
     """Home Assistant host reboot."""
     _handle(ctx, 'host/services')
 
@@ -181,18 +211,29 @@ def os(ctx: Configuration):
     ctx.auto_output('data')
 
 
-@os.command()
+@os.command('info')
 @pass_context
-def info(ctx: Configuration):
+def os_info(ctx: Configuration):
     """Home Assistant os info."""
     _handle(ctx, 'os/info')
 
 
-# @os.command()
-# @pass_context
-# def update(ctx: Configuration):
-#     """Home Assistant os update."""
-#     _handle(ctx, 'os/update', 'post')
+@os.command('update')
+@pass_context
+def os_update(ctx: Configuration):
+    """Home Assistant os update."""
+    response = _handle_raw(ctx, 'os/info')
+    data = response['data']
+    current_version = data['version']
+    latest_version = data['version_latest']
+    if StrictVersion(current_version) == StrictVersion(latest_version):
+        ctx.echo("Already running the latest release")
+    else:
+        try:
+            _handle(ctx, 'os/update', 'post')
+        except (HomeAssistantCliError, HTTPError):
+            pass
+
 
 @cli.group('hardware')
 @pass_context
@@ -222,16 +263,16 @@ def addons(ctx: Configuration):
     ctx.auto_output('data')
 
 
-@addons.command()
+@addons.command('all')
 @pass_context
-def all(ctx: Configuration):
+def addons_all(ctx: Configuration):
     """Home Assistant addons info."""
     _handle(ctx, 'addons')
 
 
-@addons.command()
+@addons.command('reload')
 @pass_context
-def reload(ctx: Configuration):
+def addons_reload(ctx: Configuration):
     """Home Assistant addons reload."""
     _handle(ctx, 'addons/reload', 'post')
 
@@ -243,78 +284,261 @@ def core(ctx: Configuration):
     ctx.auto_output('data')
 
 
-@core.command()
+@core.command('info')
 @pass_context
-def info(ctx: Configuration):
+def core_info(ctx: Configuration):
     """Home Assistant core info."""
     _handle(ctx, 'core/info')
 
 
-@core.command()
+@core.command('update')
 @pass_context
-def update(ctx: Configuration):
+def core_update(ctx: Configuration):
     """Home Assistant core update."""
-    _handle(ctx, 'core/update')
+    response = _handle_raw(ctx, 'core/info')
+    data = response['data']
+    current_version = data['version']
+    latest_version =data['version_latest']
+    if StrictVersion(current_version) == StrictVersion(latest_version):
+        ctx.echo("Already running the latest release")
+    else:
+        try:
+            _handle(ctx, 'core/update', 'post')
+        except (HomeAssistantCliError, HTTPError):
+            pass
 
 
-@core.command()
+@core.command('logs')
 @pass_context
-def logs(ctx: Configuration):
+def core_logs(ctx: Configuration):
     """Home Assistant core logs."""
     _handle(ctx, 'core/logs')
 
 
-@core.command()
+@core.command('restart')
 @pass_context
-def restart(ctx: Configuration):
+def core_restart(ctx: Configuration):
     """Home Assistant core restart."""
-    _handle(ctx, 'core/restart', 'post')
+    try:
+        _handle(ctx, 'core/restart', 'post')
+    except HomeAssistantCliError:
+        pass
 
 
-@core.command()
+@core.command('check')
 @pass_context
-def check(ctx: Configuration):
+def core_check(ctx: Configuration):
     """Home Assistant core check."""
-    _handle(ctx, 'core/check', 'post')
+    try:
+        _handle(ctx, 'core/check', 'post')
+    except (HomeAssistantCliError, HTTPError):
+        _handle(ctx, 'supervisor/logs')
 
 
-@core.command()
+@core.command('start')
 @pass_context
-def start(ctx: Configuration):
+def core_start(ctx: Configuration):
     """Home Assistant core start."""
-    _handle(ctx, 'core/start', 'post')
+    try:
+        _handle(ctx, 'core/start', 'post')
+    except HomeAssistantCliError:
+        pass
 
 
-@core.command()
+@core.command('stop')
 @pass_context
-def stop(ctx: Configuration):
+def core_stop(ctx: Configuration):
     """Home Assistant core stop."""
-    _handle(ctx, 'core/stop', 'post')
+    try:
+        _handle(ctx, 'core/stop', 'post')
+    except HomeAssistantCliError:
+        pass
 
 
-@core.command()
+@core.command('rebuild')
 @pass_context
-def rebuild(ctx: Configuration):
+def core_rebuild(ctx: Configuration):
     """Home Assistant core rebuild."""
-    _handle(ctx, 'core/rebuild', 'post')
+    try:
+        _handle(ctx, 'core/rebuild', 'post')
+    except HomeAssistantCliError:
+        pass
 
 
-@core.command()
+@core.command('options')
 @pass_context
-def options(ctx: Configuration):
+def core_options(ctx: Configuration):
     """Home Assistant core options."""
     _handle(ctx, 'core/options', 'post')
 
 
-@core.command()
+@core.command('websocket')
 @pass_context
-def websocket(ctx: Configuration):
+def core_websocket(ctx: Configuration):
     """Home Assistant core websocket."""
-    _handle(ctx, 'core/websocket')
+    try:
+        _handle(ctx, 'core/websocket')
+    except  (HomeAssistantCliError, HTTPError):
+        pass
 
 
-@core.command()
+@core.command('stats')
 @pass_context
-def stats(ctx: Configuration):
+def core_stats(ctx: Configuration):
     """Home Assistant core stats."""
     _handle(ctx, 'core/stats')
+
+
+@cli.group('dns')
+@pass_context
+def dns(ctx: Configuration):
+    """Home Assistant DNS commands."""
+    ctx.auto_output('data')
+
+
+@dns.command('info')
+@pass_context
+def dns_info(ctx: Configuration):
+    """Home Assistant DNS info."""
+    _handle(ctx, 'dns/info')
+
+
+@dns.command('options')
+@pass_context
+def dns_options(ctx: Configuration):
+    """Home Assistant DNS options."""
+    _handle(ctx, 'dns/options', 'post')
+
+
+@dns.command('restart')
+@pass_context
+def dns_restart(ctx: Configuration):
+    """Home Assistant DNS restart."""
+    try:
+        _handle(ctx, 'dns/restart', 'post')
+    except HomeAssistantCliError:
+        pass
+
+
+@dns.command('logs')
+@pass_context
+def dns_logs(ctx: Configuration):
+    """Home Assistant DNS logs."""
+    _handle(ctx, 'dns/logs')
+
+
+@dns.command('stats')
+@pass_context
+def dns_stats(ctx: Configuration):
+    """Home Assistant DNS stats."""
+    _handle(ctx, 'dns/stats')
+
+
+@cli.group('multicast')
+@pass_context
+def multicast(ctx: Configuration):
+    """Home Assistant Multicast commands."""
+    ctx.auto_output('data')
+
+
+@multicast.command('info')
+@pass_context
+def multicast_info(ctx: Configuration):
+    """Home Assistant Multicast info."""
+    _handle(ctx, 'multicast/info')
+
+
+@multicast.command('update')
+@pass_context
+def multicast_update(ctx: Configuration):
+    """Home Assistant supervisor update."""
+    response = _handle_raw(ctx, 'multicast/info')
+    data = response['data']
+    current_version = int(data['version'])
+    latest_version = int(data['version_latest'])
+    if current_version == latest_version:
+        ctx.echo("Already running the latest release")
+    else:
+        try:
+            _handle(ctx, 'multicast/update', 'post')
+        except (HomeAssistantCliError, HTTPError):
+            pass
+
+
+@multicast.command('restart')
+@pass_context
+def dns_restart(ctx: Configuration):
+    """Home Assistant Multicast restart."""
+    try:
+        _handle(ctx, 'multicast/restart', 'post')
+    except HomeAssistantCliError:
+        pass
+
+
+@multicast.command('logs')
+@pass_context
+def multicast_logs(ctx: Configuration):
+    """Home Assistant DNS logs."""
+    _handle(ctx, 'multicast/logs')
+
+
+@multicast.command('stats')
+@pass_context
+def multicast_stats(ctx: Configuration):
+    """Home Assistant Multicast stats."""
+    _handle(ctx, 'multicast/stats')
+
+
+@cli.group('ha-cli')
+@pass_context
+def ha_cli(ctx: Configuration):
+    """Home Assistant ha-cli commands."""
+    ctx.auto_output('data')
+
+
+@ha_cli.command('info')
+@pass_context
+def ha_info(ctx: Configuration):
+    """Home Assistant ha-cli info."""
+    _handle(ctx, 'cli/info')
+
+
+@ha_cli.command('update')
+@pass_context
+def ha_update(ctx: Configuration):
+    """Home Assistant supervisor update."""
+    response = _handle_raw(ctx, 'cli/info')
+    data = response['data']
+    current_version = int(data['version'])
+    latest_version = int(data['version_latest'])
+    if current_version == latest_version:
+        ctx.echo("Already running the latest release")
+    else:
+        try:
+            _handle(ctx, 'cli/update', 'post')
+        except (HomeAssistantCliError, HTTPError):
+            pass
+
+
+@ha_cli.command('restart')
+@pass_context
+def ha_restart(ctx: Configuration):
+    """Home Assistant ha-cli restart."""
+    try:
+        _handle(ctx, 'cli/restart', 'post')
+    except HomeAssistantCliError:
+        pass
+
+
+@ha_cli.command('logs')
+@pass_context
+def ha_logs(ctx: Configuration):
+    """Home Assistant ha-cli logs."""
+    _handle(ctx, 'cli/logs')
+
+
+@ha_cli.command('stats')
+@pass_context
+def ha_stats(ctx: Configuration):
+    """Home Assistant ha-cli stats."""
+    _handle(ctx, 'cli/stats')
