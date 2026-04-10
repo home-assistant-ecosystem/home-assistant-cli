4f7f474477e3e3f13ddbee7c0c562377ba5c5762
Fabian Affolter <mail@fabian-affolter.ch>
2026-04-10 20:34:45 +0200

    Sync quotes with other files

--- homeassistant_cli/const.py
+++ homeassistant_cli/const.py
@@ -1,21 +1,22 @@
 """Constants used by Home Assistant CLI (hass-cli)."""
-PACKAGE_NAME = 'homeassistant_cli'
 
-__version__ = '1.0.0'
+PACKAGE_NAME = "homeassistant_cli"
 
-AUTO_SERVER = 'auto'
-DEFAULT_SERVER = 'http://localhost:8123'
-DEFAULT_SERVER_MDNS = 'http://homeassistant.local:8123'
+__version__ = "1.0.0"
+
+AUTO_SERVER = "auto"
+DEFAULT_SERVER = "http://localhost:8123"
+DEFAULT_SERVER_MDNS = "http://homeassistant.local:8123"
 DEFAULT_TIMEOUT = 5
-DEFAULT_OUTPUT = 'json'  # TODO: Have default be human table relevant output
+DEFAULT_OUTPUT = "json"  # TODO: Have default be human table relevant output
 
-DEFAULT_DATAOUTPUT = 'yaml'
+DEFAULT_DATAOUTPUT = "yaml"
 
-COLUMNS_DEFAULT = [('ALL', '$')]
+COLUMNS_DEFAULT = [("ALL", "$")]
 COLUMNS_ENTITIES = [
-    ('ENTITY', 'entity_id'),
-    ('DESCRIPTION', 'attributes.friendly_name'),
-    ('STATE', 'state'),
-    ('CHANGED', 'last_changed'),
+    ("ENTITY", "entity_id"),
+    ("DESCRIPTION", "attributes.friendly_name"),
+    ("STATE", "state"),
+    ("CHANGED", "last_changed"),
 ]
-COLUMNS_SERVICES = [('DOMAIN', 'domain'), ("SERVICE", "domain.services[*]")]
+COLUMNS_SERVICES = [("DOMAIN", "domain"), ("SERVICE", "domain.services[*]")]
